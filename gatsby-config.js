5a6bcdf8e42ee62ffb3ac0a7bafc8fdad1976a5d
Mark Jackson <markjackson02@ymail.com>
2020-10-17 16:01:07 -0400

    wip [skip ci]

--- gatsby-config.js
+++ gatsby-config.js
@@ -46,7 +46,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: process.env.GOOGLE_ANALYTICS_ID,
+        trackingId: "UA-167342385-1",
       },
     },
     `gatsby-plugin-sitemap`,

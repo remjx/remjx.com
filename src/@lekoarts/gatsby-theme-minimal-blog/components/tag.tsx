8ee08df41f9ff92913108142adb892fc61c48530
Mark Jackson <markjackson02@ymail.com>
2020-10-25 17:00:42 -0400

    update override components

--- src/@lekoarts/gatsby-theme-minimal-blog/components/tag.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/tag.tsx
@@ -1,11 +1,7 @@
 /** @jsx jsx */
 import { jsx, Heading, Link as TLink } from "theme-ui"
-import { Flex } from "@theme-ui/components"
-import { Link } from "gatsby"
 import Layout from "@lekoarts/gatsby-theme-minimal-blog/src/components/layout"
-import useMinimalBlogConfig from "@lekoarts/gatsby-theme-minimal-blog/src/hooks/use-minimal-blog-config"
 import Listing from "@lekoarts/gatsby-theme-minimal-blog/src/components/listing"
-import replaceSlashes from "@lekoarts/gatsby-theme-minimal-blog/src/utils/replaceSlashes"
 import SEO from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo"
 
 type TagProps = {
@@ -30,14 +26,12 @@ type TagProps = {
 }
 
 const Tag = ({ posts, pageContext }: TagProps) => {
-  const { tagsPath, basePath } = useMinimalBlogConfig()
-
   return (
     <Layout>
       <SEO title={`Tag: ${pageContext.name}`} />
-      <Flex sx={{ alignItems: `center`, justifyContent: `space-between`, flexFlow: `wrap` }}>
-        <Heading variant="styles.h2">{pageContext.name}</Heading>
-      </Flex>
+      <Heading as="h1" variant="styles.h1" sx={{ marginY: 2 }}>
+        {pageContext.name}
+      </Heading>
       <Listing posts={posts} sx={{ mt: [4, 5] }} />
     </Layout>
   )
